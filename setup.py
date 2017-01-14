886dc5ff05def1ee0a7deb708b5ffe67edb58a1a
Kramer Michael Quist <Kramer@Kramers-MacBook-Pro.local>
2017-01-14 17:09:07 -0500

    classifier change

--- setup.py
+++ setup.py
@@ -9,5 +9,19 @@ setup(
   url = 'https://github.com/kquist/DoubleML-Python',
   download_url = 'https://github.com/peterldowns/DoubleML-Python/0.1', 
   keywords = ['Double machine learning', 'Neyman machine learning', 'orthogonalization', 'cross-fit machine learning', 'de-biased machine learning']
-  classifiers = [],
+  classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Economists and Statisticians',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3.5',
+],
 )
