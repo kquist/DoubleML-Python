80b5f01d4a4882469399abea329a0d1941213fea
kquist <kramer.quist@gmail.com>
2017-01-14 16:58:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'The DoubleML package is intended to be used to implement the estimation procedure developed in "Double Machine  Learning for Treatment and Causal Parameters" by Victor Chernozhukov, Denis Chetverikov, Mert Demirer, Esther Duflo, Christian Hansen, and Whitney Newey.',
   author = 'Kramer Quist',
   author_email = 'kramer.quist@gmail.com',
-  url = 'https://github.com/kquist/DoubleML/',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', 
+  url = 'https://github.com/kquist/DoubleML-Python',
+  download_url = 'https://github.com/peterldowns/DoubleML-Python/0.1', 
   keywords = ['Double machine learning', 'Neyman machine learning', 'orthogonalization', 'cross-fit machine learning', 'de-biased machine learning']
   classifiers = [],
 )
